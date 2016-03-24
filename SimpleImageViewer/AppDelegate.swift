fc580e0297b91e07d36d416d68341849dcb16e2a
Phoenix <phyrworks@gmail.com>
2016-03-24 14:52:08 -0700

    Initial Commit

---
+++ SimpleImageViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SimpleImageViewer
+//
+//  Created by Phoenix on 3/24/16.
+//  Copyright © 2016 Phoenix Toews. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SimpleImageViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SimpleImageViewer
+//
+//  Created by Phoenix on 3/24/16.
+//  Copyright © 2016 Phoenix Toews. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
